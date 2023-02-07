6c2640813462e53e572a9fa075e1915e36cfab82
Prateek Verghese <prateek@2Prateeks-MacBook-Pro.local>
2023-02-07 00:00:27 -0800

    push

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,11 +4,12 @@ const Hero =() => {
     return(
         <div className="text-white"> 
         
-        <div className="max-w-[800px] mt-[px] w-full h-screen mx-auto text-center flex flex-col justify-center"></div>
+        <div className="max-w-[800px] mt-[px] w-full h-screen mx-auto text-center flex flex-col justify-center">
         <p className="text-[#00df9a] font-bold p2 ">I am the Best</p>
         <h1 className="md:text-7xl sm:text-6xl"></h1>
         <div>
             <p className="md:text-7xl sm:text-6xl">j</p>
+        </div>
         </div>
         </div>
         
