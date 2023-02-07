8821b0ebb49df43113f97735aace365012a19465
Prateek Verghese <prateek@2Prateeks-MacBook-Pro.local>
2023-02-06 23:56:32 -0800

    added Css NavBar and some hero stuff

---
+++ src/components/Hero.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Hero =() => {
+    return(
+        <div className="text-white"> 
+        
+        <div className="max-w-[800px] mt-[px] w-full h-screen mx-auto text-center flex flex-col justify-center"></div>
+        <p className="text-[#00df9a] font-bold p2 ">I am the Best</p>
+        <h1 className="md:text-7xl sm:text-6xl"></h1>
+        <div>
+            <p className="md:text-7xl sm:text-6xl">j</p>
+        </div>
+        </div>
+        
+    )
+    
+    
+}
+export default Hero
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,33 @@
-import React from "react";
-import {AiOutlineclose, AiOutlineMenu} from 'react-icons/ai';
+import React , {useState} from "react";
+import {AiOutlineClear, AiOutlineClose, AiOutlineMenu} from 'react-icons/ai';
 const Navbar=() => {
+    const [nav, setNav]= useState(true)
+
+    const handleNav = () =>{
+        setNav(!nav)
+    }
+
     return(
         <div className="flex justify-between items-center h-24 max-w-[1240px] mx-auto text-white">
-            <h1 className="w-full text-3xl font-bold text-[#00df9a]">REACT.</h1>
-            <ul className="flex hidden">
+            <h1 className="w-full text-3xl font-bold text-[#00df9a]">REACTPORTFOLIO.</h1>
+            <ul className="hidden sm:flex">
                 <li className="p-4">About</li>
                 <li className="p-4"> MYProjects</li>
-                <li className="p-4">yike</li>
-                <li className="p-4">Home</li>
+                <li className="p-4">ContactMe</li>
+                <li className="p-4">HireMe</li>
             </ul>
-            <div>
-                <AiOutlineMenu size={20}/>
+            <div onClick={handleNav} className="block sm:hidden">
+                { !nav ? <AiOutlineClose size ={20}/>  : <AiOutlineMenu size={20}/>}
+                
             </div>
-            <div className="fixed left-0 top-0 w-[60%] border-r border-r-gray-900 bg-[#00300]">
-                <ul className="pt-24 uppercase p-4 " >
-                <li className="p-4">About</li>
-                <li className="p-4"> MYProjects</li>
-                <li className="p-4">yike</li>
-                <li className="p-4">Home</li>
+            
+            <div className= {!nav ? 'fixed left-0 top-0 w-[50%] border-r border-r-gray-900 bg-[#00300]' : 'fixed left-[-100%]'}>
+            <h1 className="w-full text-3xl font-bold text-[#00df9a]">REACT.</h1>
+                <ul className="uppercase p-4" >
+                <li className="p-4 border-b border-gray-600" >About</li>
+                <li className="p-4 border-b border-gray-600"> MYProjects</li>
+                <li className="p-4 border-b border-gray-600">yike</li>
+                <li className="p-4 border-b border-gray-600">Home</li>
                 </ul>
             </div>
         </div>
